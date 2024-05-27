dea7298891d0e4f4251319b0ca8eb52196ea1772
MasterHell <123289351+MasterMaybeFromHell@users.noreply.github.com>
2024-05-27 14:21:02 +0300

    Add files via upload

--- MainScript.cs
+++ MainScript.cs
@@ -7,23 +7,27 @@ namespace IPChecker
     public class MainScript
     {
         public static string _ip = "";
+        public static int _port = 0;
         public static bool _serverIsRunning = false;
-        public static Ping _server = new Ping();
 
         public static void Main()
         {
-            Console.WriteLine("Enter the IP and port of the server you want to check: ");
+            Console.WriteLine("Enter the IP: ");
             _ip = Console.ReadLine();
+            Console.WriteLine("Enter the Port: ");
+            _port = Console.Read();
             Console.Clear();
             while (!_serverIsRunning)
             {
                 try
                 {
-                    _server.Send(_ip);
-                    Console.WriteLine("Server is running");
-                    _serverIsRunning = true;
+                    using (TcpClient server = new TcpClient(_ip, _port))
+                    {
+                        Console.WriteLine("Server is running");
+                        _serverIsRunning = true;
+                    }
                 }
-                catch
+                catch (SocketException ex)
                 {
                     Console.WriteLine("Server is offline");
                     Thread.Sleep(100000);
